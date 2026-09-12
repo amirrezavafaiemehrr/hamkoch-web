c4c00e191e18b2b32cd20adc160d6ffbde97c408
amirrezavafaiemehrr <amirrezavafaiemehrr@gmail.com>
2026-09-12 09:32:06 +0330

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || 'https://rdifdufgzizwrefpeojj.supabase.co';
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJkaWZkdWZneml6d3JlZnBlb2pqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODg2NzM4NTcsImV4cCI6MjEwNDI0OTg1N30.nX8LxxYyFVoIRvHVFHfUryO_SxYOrtXK7aqmDbvHnoA';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
